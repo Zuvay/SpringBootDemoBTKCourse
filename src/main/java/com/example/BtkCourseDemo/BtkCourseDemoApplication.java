e70a5130492afb8eae2314808ea3b41d03e40498
ÖmerFaruk <69796388+Zuvay@users.noreply.github.com>
2024-07-22 02:26:01 +0300

    Proje başlatıldı

---
+++ src/main/java/com/example/BtkCourseDemo/BtkCourseDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.BtkCourseDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtkCourseDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtkCourseDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BtkCourseDemo/BtkCourseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BtkCourseDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtkCourseDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
